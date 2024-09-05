04caaab45ee67a5cef04bae7ed7a4de1c0f8c863
CanerYesiltas <caneryesiltas1@gmail.com>
2024-09-04 21:59:04 -0400

    handclick function and event added

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "react-bootstrap";
 import { MdFavorite } from "react-icons/md";
 
-const ProductCard = ({id,title,price, description,image}) => {
+const ProductCard = ({ title, price, description, image }) => {
+  const [heart, setHeart] = useState(false);
+
+  const handleClick = () => setHeart(!heart);
+
   return (
     <Card className="rounded-2 m-auto card" role="button">
       <Card.Header className="d-flex justify-content-between">
-        <Card.Title>price</Card.Title>
-        <MdFavorite size={30} />
+        <Card.Title>{price}</Card.Title>
+        <MdFavorite size={30} onClick={handleClick} className={heart? "text-danger": ""} />
       </Card.Header>
-      <Card.Img variant="top" src="" className="player-logo" />
+      <Card.Img variant="top" src={image} className="player-logo" />
 
       <Card.Footer className="card__over">
-        <Card.Title>title</Card.Title>
+        <Card.Title>{title}</Card.Title>
       </Card.Footer>
     </Card>
   );
